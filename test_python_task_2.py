78bccfba9a73055d7c2361c120d379af9e862357
Bill Imus <bilimus@gmail.com>
2018-04-09 03:30:01 +0500

    Group class created
    done

--- test_python_task_2.py
+++ test_python_task_2.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
+from group import Group
 
 
 def is_alert_present(wd):
@@ -23,7 +24,7 @@ class test_python_group_task_1(unittest.TestCase):
         wd = self.open_page()
         self.login(wd, user = "admin", password = "secret")
         self.open_groups(wd)
-        self.create_group(wd, name = "sdfer", header = "dferty", footer = "sderty")
+        self.create_group(wd, Group(name = "sdfer", header = "dferty", footer = "sderty"))
         self.return_page(wd)
         self.log_out(wd)
         self.assertTrue(success)
@@ -34,17 +35,17 @@ class test_python_group_task_1(unittest.TestCase):
     def return_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def open_groups(self, wd):
