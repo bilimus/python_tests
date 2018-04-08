085e8d389bc53b6a440c4940eb085d0ad0d6cb05
Bill Imus <bilimus@gmail.com>
2018-04-09 02:23:14 +0500

    refactoring login
    login method

---
+++ test_python_task_2.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+
+class test_python_group_task_1(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(capabilities={"marionette": False},\
+                            firefox_binary="C:/Program Files/FirefoxESR/firefox.exe")
+        self.wd.implicitly_wait(60)
+
+    def test_test_python_group_task_1(self):
+        success = True
+        wd = self.open_page()
+        self.login(wd)
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("sdfer")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("dferty")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("sderty")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("Logout").click()
+        self.assertTrue(success)
+
+    def open_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        return wd
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
