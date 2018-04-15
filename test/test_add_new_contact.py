18920f3d0665787a314fb116eb1a965f4f8c491d
Bill Imus <bilimus@gmail.com>
2018-04-16 03:05:15 +0500

    Выделен класс для работы с контактами

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -14,11 +14,11 @@ def app(request):
 
 def test_add_new_contact(app):
     app.session.login(username = "admin", password = "secret")
-    app.adding_new_contact(Contact(firstname = 'Bill', middlename = 'Bill', lastname ='Imus',\
-        nickname ='Billimus', title = 'QA Tester', company ='QA Solutions', address ='Somewhere in the Universe',\
-        home_phone ='+122222222', mobile ='+13333333', work_phone = '+14444444', fax ='+15555555',\
-        email_1 ='Bill_1@qa.com', email_2 ='Bill_2@qa.com', email_3 ='Bill_2@qa.com', homepage ='IhaveNoPage.com',\
-        byear ='2000',ayear='2001', city ='Sim City', phone2 ='+156666666', notes_here ='some notes here'))
+    app.contact.add(Contact(firstname ='Bill', middlename ='Bill', lastname ='Imus', \
+                            nickname ='Billimus', title = 'QA Tester', company ='QA Solutions', address ='Somewhere in the Universe', \
+                            home_phone ='+122222222', mobile ='+13333333', work_phone = '+14444444', fax ='+15555555', \
+                            email_1 ='Bill_1@qa.com', email_2 ='Bill_2@qa.com', email_3 ='Bill_2@qa.com', homepage ='IhaveNoPage.com', \
+                            byear ='2000', ayear='2001', city ='Sim City', phone2 ='+156666666', notes_here ='some notes here'))
     app.session.logout()
 
 
