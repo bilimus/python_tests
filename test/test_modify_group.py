2fd387985a5692bc37b69a9b109938dfa9d47a72
Bill Imus <bilimus@gmail.com>
2018-04-23 01:43:07 +0500

    Задание 8. name, header modified

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,15 @@ from model.group import Group
 
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.open_groups_page()
     app.group.modify_first_group(Group(name="name modified", header="header modified", footer="footer modified"))
-    app.group.return_to_group_page()
+    app.session.logout()
+
+def test_modify_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="name modified"))
+    app.session.logout()
+
+def test_modify_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="header modified"))
     app.session.logout()
