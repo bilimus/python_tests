3b067ce8d1501b9596f927b25d150fda501c225e
Bill Imus <bilimus@gmail.com>
2018-04-23 02:20:04 +0500

    Задание 8. Footer modified

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -14,3 +14,9 @@ def test_modify_first_group_header(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="header modified"))
     app.session.logout()
+
+def test_modify_first_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="footer modified"))
+    app.session.logout()
+
