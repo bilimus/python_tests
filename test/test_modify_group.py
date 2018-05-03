949ab525d0e9000a29f291b1e942cce72a0479a8
Bill Imus <bilimus@gmail.com>
2018-05-03 11:51:20 +0500

    Задание 11. Убраны комментарии.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -7,32 +7,16 @@ def test_modify_first_group(app):
     app.group.modify_first_group(Group(name="name modified", header="header modified", footer="footer modified"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    #old_list = sorted(old_groups, key=lambda p: int(p.id))
-    #new_list = sorted(new_groups, key=lambda p: int(p.id))
-
-    print(' --==groups before if==-- ')
-    print(old_groups)
-    print(new_groups)
 
     if old_groups[0].id == new_groups[-1].id:
         old_list = old_groups[1:]
         new_list = new_groups[:-1]
-        print('--==if is true==-')
-        print(old_list)
-        print(new_list)
     elif old_groups[0].id == new_groups[0].id:
         old_list = old_groups[1:]
         new_list = new_groups[1:]
-        print('-=if is false elif=-')
-        print(old_list)
-        print(new_list)
     else:
         old_list = old_groups
         new_list = new_groups
-        print('-=else=-')
-        print(old_list)
-        print(new_list)
-
     assert old_list == new_list
 
 def test_modify_first_group_name(app):
@@ -42,29 +26,15 @@ def test_modify_first_group_name(app):
     app.group.modify_first_group(Group(name="name modified"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    print(' --==groups before if==-- ')
-    print(old_groups)
-    print(new_groups)
-
     if old_groups[0].id == new_groups[-1].id:
         old_list = old_groups[1:]
         new_list = new_groups[:-1]
-        print('--==if is true==-')
-        print(old_list)
-        print(new_list)
     elif old_groups[0].id == new_groups[0].id:
         old_list = old_groups[1:]
         new_list = new_groups[1:]
-        print('-=if is false elif=-')
-        print(old_list)
-        print(new_list)
     else:
         old_list = old_groups
         new_list = new_groups
-        print('-=else=-')
-        print(old_list)
-        print(new_list)
-
     assert old_list == new_list
 
 
