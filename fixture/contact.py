5808c91118b3aa7075202eaf5b775a55755d0af4
Bill Imus <bilimus@gmail.com>
2018-05-10 22:58:02 +0500

    Задание 14. Ошибки исправлены. Склейка номеров выполнена.
    видео 5_3 (концовка)

--- fixture/contact.py
+++ fixture/contact.py
@@ -138,19 +138,19 @@ class ContactHelper:
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, mobilephone=mobilephone,
-                       workphone=workphone, fax=secondaryphone)
+        return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, workphone=workphone,
+                       mobilephone=mobilephone, secondaryphone=secondaryphone)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
         self.open_contact_view_by_index(index)
         text = wd.find_element_by_id('content').text
         homephone = re.search("H: (.*)", text).group(1)
-        workphone = re.search("W: (.*)", text).group(1)
         mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
         secondaryphone = re.search("P: (.*)", text).group(1)
         return Contact(homephone=homephone, mobilephone=mobilephone,\
-                       workphone=workphone, fax=secondaryphone)
+                       workphone=workphone, secondaryphone=secondaryphone)
 
 
 
