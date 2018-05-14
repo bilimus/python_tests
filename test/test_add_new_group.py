39f31a872222c86179490d1d5c6639eeedd9e6b0
Bill Imus <bilimus@gmail.com>
2018-05-14 11:00:41 +0500

    Задание 17: Реализована загрузка информации о тестируемом приложении из конфигурационного файла и опция командной строки, позволяющая передавать путь к этому конфигурационному файлу

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -24,14 +24,3 @@ def test_add_new_group(app, group):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-'''
-def test_add_new_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    old_groups.append(group)
-    new_groups = app.group.get_group_list()
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-'''
