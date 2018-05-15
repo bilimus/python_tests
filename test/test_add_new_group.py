e95a5b9e885de9fa5b2eb20ef3a95522d7bd2cfb
Bill Imus <bilimus@gmail.com>
2018-05-15 22:19:48 +0500

    Задание 18: Реализован генератор тестовых данных для групп. (Есть ошибки)
    Выдает ошибку: C:\GitHub\python_testing\venv\Scripts\python.exe C:/GitHub/python_testing/generator/group.py -n10 -f data/test.json
    Traceback (most recent call last):
      File "C:/GitHub/python_testing/generator/group.py", line 35, in <module>
        with open(file, "w") as out:
    FileNotFoundError: [Errno 2] No such file or directory: 'C:\\GitHub\\python_testing\\generator\\..\\'
    
    Process finished with exit code 1

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -2,18 +2,9 @@
 # test_python_task_4
 
 from model.group import Group
-from sys import maxsize
 import pytest
-import random
-import string
+from data.add_group import constant as testdata
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Group(name="", header="", footer="")] + [\
-    Group(name=random_string("name", 10).strip(), header=random_string("header", 20).strip(), footer=random_string("footer", 20).strip())
-    for i in range(5)]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x)for x in testdata])
 
