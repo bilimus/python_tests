4f137eb55eb6f4feb57607d1de9f22a19ae0f1c7
Bill Imus <bilimus@gmail.com>
2018-05-22 12:01:14 +0500

    Задание 20. 2 проблемы
    видео 7_4
    2 проблемы:
    1. Тесты падают в test_add_new_group
    2. В видео не показана реализация метода remove(group) в тесте test_del_group.py

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,18 @@
 from model.group import Group
+import random
+
+def test_delete_some_group(app, db):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name='test'))
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == app.group.count()
+    old_groups.remove(group)
+    assert old_groups == new_groups
+'''
+from model.group import Group
 from random import randrange
 
 def test_delete_some_group(app):
@@ -11,4 +25,4 @@ def test_delete_some_group(app):
     new_groups = app.group.get_group_list()
     old_groups[index:index+1]=[]
     assert old_groups == new_groups
-
+'''
