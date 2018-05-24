c1fe7848094a48b9bd707cd2945fad63b00cb185
Bill Imus <bilimus@gmail.com>
2018-05-24 09:41:27 +0500

    Задание 20
    Найдена ошибка в test_del_group.py (11 line)
    исправлена.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -8,7 +8,7 @@ def test_delete_some_group(app, db, check_ui):
     group = random.choice(old_groups)
     app.group.delete_group_by_id(group.id)
     new_groups = db.get_group_list()
-    assert len(old_groups) - 1 == app.group.count()
+    assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
     if check_ui:
