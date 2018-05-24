ca70e95b19f75e7bae638dd58619d60493d83632
Bill Imus <bilimus@gmail.com>
2018-05-24 09:17:21 +0500

    Задание 20
    Начинаем переделывать test_modify_group.py согласно заданию.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -16,6 +16,7 @@ def test_modify_group_name(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+
 def test_modify_first_group(app):
     group = Group(name="name modified", header="header modified", footer="footer modified")
     if app.group.count() == 0:
@@ -28,6 +29,20 @@ def test_modify_first_group(app):
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
+'''
+def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="name test"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(name="name modified")
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
+    assert len(old_groups) == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups[index] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+'''
 '''
 def test_modify_first_group_header(app):
     group = Group(header="header test")
